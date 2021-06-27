4f0a1e17fb33326aa99c5ada1169481c98cbb886
Pascal Le Merrer <pascal.lemerrer@protonmail.com>
2021-06-27 16:51:41 +0200

    Add Browser Document app

--- src/PhotoGallery.elm
+++ src/PhotoGallery.elm
@@ -1,5 +1,44 @@
 module PhotoGallery exposing (..)
 
-import Html exposing (h1, text)
-main  =
-    h1 [][text "Photo Gallery"]
+import Browser
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (class)
+type alias Model = {}
+
+type Msg = NoOp
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ({}, Cmd.none)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    (model, Cmd.none)
+
+main : Program () Model Msg
+main =
+    Browser.document
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = \_ -> Sub.none
+    }
+
+
+view : Model -> Browser.Document Msg -- 1
+view model =
+    { title = "Photo Gallery"
+    , body = [ viewBody model]
+    }
+viewBody : Model -> Html Msg
+viewBody model =
+    div [ class "container" ]
+        [ viewTitle ]
+
+viewTitle : Html Msg
+viewTitle =
+    h1 [ class "title is-1" ]
+        [ text "Photo Gallery" ]
+
+
